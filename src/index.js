11f8cd43f9685ca56381519ded924893c10fc92f
Krzysztof Waszkiewicz <kwaszkiewicz94@gmail.com>
2020-10-14 10:59:42 +0200

    initiate first routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+    return ( <Router>
+        <Switch>
+            <Route exact path="/" component = {App} />
+            <Route path="/Login" component = {Login} />
+            <Route path="/Register" component = {Register} />
+
+        </Switch>
+    </Router> );
+}
+ 
+export default Root;
+
+ReactDOM.render(<Root/>, document.getElementById('root'));
 registerServiceWorker();
