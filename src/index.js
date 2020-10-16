9840fa881bb583e37a61db89ff47c45a7e551442
Krzysztof Waszkiewicz <kwaszkiewicz94@gmail.com>
2020-10-16 15:39:56 +0200

    implement redux store to keep signed user info

--- src/index.js
+++ src/index.js
@@ -8,13 +8,22 @@ import {BrowserRouter as Router, Switch, Route, withRouter, useHistory } from 'r
 import 'semantic-ui-css/semantic.min.css'
 import firebase from './firebase';
 
+import {createStore} from 'redux';
+import {Provider, useDispatch} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension'
+import rootReducer from './reducers/';
+import {setUser} from './actions';
+
+const store = createStore(rootReducer, composeWithDevTools());
 const Root = () => {
+    
+    const dispatch = useDispatch();
     const history = useHistory();
-    console.log(history);
     
     useEffect(()=>{
         firebase.auth().onAuthStateChanged(user => {
             if(user){
+                dispatch(setUser(user));
                 const location = {
                     pathname: "/",
                 }
@@ -23,16 +32,26 @@ const Root = () => {
         })
     }, []);
 
-    return ( 
+    return (
+        
         <Switch>
             <Route exact path="/" component = {App} />
             <Route path="/Login" component = {Login} />
             <Route path="/Register" component = {Register} />
         </Switch>
+        
          );
 }
- const RootWithAuth =  withRouter(Root);
-export default Root;
+const RootWithAuth =  withRouter(Root);
 
-ReactDOM.render(<Router><RootWithAuth /></Router>, document.getElementById('root'));
+ReactDOM.render(
+    (
+        <Provider store={store}>
+            <Router>
+                <RootWithAuth />
+            </Router>
+        </Provider>
+    ), document.getElementById('root')
+);
+
 registerServiceWorker();
