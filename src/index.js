8bdc1deb9f68477dca8ac7960541c0986510d4d6
Krzysztof Waszkiewicz <kwaszkiewicz94@gmail.com>
2020-10-16 12:51:09 +0200

    setup firebase to listen for Login/Register

--- src/index.js
+++ src/index.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, withRouter, useHistory } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css'
+import firebase from './firebase';
 
 const Root = () => {
-    return ( <Router>
+    const history = useHistory();
+    console.log(history);
+    
+    useEffect(()=>{
+        firebase.auth().onAuthStateChanged(user => {
+            if(user){
+                const location = {
+                    pathname: "/",
+                }
+                history.push(location);
+            }
+        })
+    }, []);
+
+    return ( 
         <Switch>
             <Route exact path="/" component = {App} />
             <Route path="/Login" component = {Login} />
             <Route path="/Register" component = {Register} />
-
         </Switch>
-    </Router> );
+         );
 }
- 
+ const RootWithAuth =  withRouter(Root);
 export default Root;
 
-ReactDOM.render(<Root/>, document.getElementById('root'));
+ReactDOM.render(<Router><RootWithAuth /></Router>, document.getElementById('root'));
 registerServiceWorker();
