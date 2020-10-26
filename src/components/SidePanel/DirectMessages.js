4b11d05119b7d9d4ff21e2196d8bdc29f87d68a5
Krzysztof Waszkiewicz <kwaszkiewicz94@gmail.com>
2020-10-26 22:34:29 +0100

    final implementation with comments

--- src/components/SidePanel/DirectMessages.js
+++ src/components/SidePanel/DirectMessages.js
@@ -1,8 +1,123 @@
-import React, {useState} from 'react';
-
-import { Menu, Icon} from 'semantic-ui-react'
+import React, {useState, useEffect, useRef} from 'react';
+import {useSelector} from 'react-redux';
+import { Menu, Icon} from 'semantic-ui-react';
+import firebase from '../../firebase';
 const DirectMessages = () => {
+    const currentUser = useSelector(store => store.user.currentUser);
     const [users, setUsers] = useState([]);
+    const [lastUser, setLastUser] = useState(null);
+    const [readyToListen, setReadyToListen] = useState(false);
+    const usersDbRef = firebase.database().ref('users');
+    const connectedRef = firebase.database().ref('.info/connected');
+    const presenceDbRef = firebase.database().ref('presence');
+    
+    useEffect(()=>{
+        if(currentUser){
+            usersDbRef.limitToLast(1).on('child_added', snap => {
+            if(lastUser === null){
+                setLastUser(snap.val());
+                //the value of lastUser is necessary to check when
+                //the program has finished loading all currently
+                //registered users
+            }
+        })}
+    },[])
+    useEffect(()=>{
+        if(currentUser){
+            let loadedUsers = [];
+            usersDbRef.on("child_added", snap => {
+                let user = snap.val();
+                user["uid"] = snap.key;
+                user["status"] = "offline";
+                loadedUsers.push(user);
+                setUsers([...loadedUsers]);
+        });
+        }
+    }, [])
+
+
+    useEffect(()=>{
+        //addListeners function suppose to run only after all of the users from the
+        //firebase database are loaded into users state
+
+        //readyToListen indicates the readiness and its state is going to be
+        //changed only once, which ensures that this effect won't be 
+        //executed multiple times
+        if(currentUser && readyToListen){
+            addListeners(currentUser.uid)
+        }
+    }, [readyToListen])
+
+    if(users.length>0 && lastUser !== null && !readyToListen){
+        if(users[users.length-1].uid === lastUser.uid){
+        setReadyToListen(true);
+    }}
+
+    const addListeners = currentUserUid => {
+
+        //1st listener
+        //while logging into the app, adds user's ID into database >> presence
+        connectedRef.on('value', snap => {
+            if(snap.val() === true){
+                const ref = presenceDbRef.child(currentUserUid);
+                ref.set(true);
+                ref.onDisconnect().remove(err => {
+                    if(err!==null){
+                        console.error(err);
+                    }
+                })
+            }
+        })
+
+        //2nd listener
+        //checks when the first listener adds user's ID into database >> presence, then executes function which changes user's status to ONLINE
+        presenceDbRef.on('child_added', snap => {
+            if(currentUserUid !== snap.key) {
+                addStatusToUser(snap.key);
+            }
+        })
+
+        //3rd listener
+        //same as 2nd listener but it checks when the ID from database is removed, then it executes the addStatusToUser to change user's status to OFFLINE
+        presenceDbRef.on('child_removed', snap => {
+            if(currentUserUid !== snap.key) {
+                addStatusToUser(snap.key, false);
+            }
+        })
+    }
+
+    const addStatusToUser = (userId, connected = true) => {
+        const updatedUsers = users.reduce((acc, user) => {
+            if(user.uid === userId){
+                user['status'] = `${connected ? 'online' : 'offline'}`
+            }
+            return acc.concat(user);
+        }, [])
+        setUsers([...updatedUsers]);
+    }
+
+    const isUsersOnline = (user) => {
+        return user.status === 'online' ;
+    }
+
+    const displayUsers = () =>{
+        return users.map(user => {
+            if(user.uid!==currentUser.uid) return (
+            <Menu.Item
+            key={user.uid}
+            onClick = {()=>{console.log(user)}}
+            style={{opacity: 0.7, fontStyle: 'italic'}}
+            
+            >
+                <Icon
+                name='circle'
+                color={isUsersOnline(user) ? 'green' : 'blue'}
+                />
+
+                @ {user.name}
+            </Menu.Item>
+        )})
+    }
     return ( 
         <Menu.Menu className="menu">
             <Menu.Item>
@@ -11,8 +126,7 @@ const DirectMessages = () => {
                 </span>
                 ({users.length})
             </Menu.Item>
-            {/* Users to send direct Messages */}
-
+            {displayUsers()}
         </Menu.Menu>
      )
 }
