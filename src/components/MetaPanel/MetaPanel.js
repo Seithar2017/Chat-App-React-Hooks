55eab280bccab1187ffa4fd6ede538d7d5bf3187
Krzysztof Waszkiewicz <kwaszkiewicz94@gmail.com>
2020-11-04 22:41:06 +0100

    add displayTopPosters function

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,14 +1,33 @@
 import React, {useState} from 'react';  
-import {Segment, Accordion, Header, Icon, Image} from 'semantic-ui-react';
+import {Segment, Accordion, Header, Icon, Image, List} from 'semantic-ui-react';
 import {useSelector} from 'react-redux';
 const MetaPanel = () => {
     const [activeIndex, setActiveIndex] = useState(0);
     const isPrivateChannel = useSelector(store => store.channel.isPrivateChannel);
     const currentChannel = useSelector(store => store.channel.currentChannel);
+    const userPosts = useSelector(store => store.channel.userPosts);
     const handleSetActiveIndex = (event, titleProps) => {
         const {index} = titleProps;
         const newIndex = activeIndex === index ? -1 : index;
         setActiveIndex(newIndex);
+    }
+
+    const displayTopPosters = (userPosts) => {
+        const posts = Object.entries(userPosts);
+        const sortedPosts = posts.sort((a, b) => b[1].count - a[1].count);
+        return sortedPosts.map(([key, val], i) => {
+                
+            return (
+                <List.Item
+                  key={i}
+                >
+                    <Image avatar src={val.avatar} />
+                    <List.Content>
+                        <List.Header as="a">{key}</List.Header>
+                        <List.Description>{val.count} {val.count > 1 ? 'posts' : 'post'}</List.Description>
+                    </List.Content>
+                </List.Item>
+            )}).slice(0, 5);
     }
     return !isPrivateChannel && ( 
         
@@ -45,7 +64,9 @@ const MetaPanel = () => {
                     <Icon name = "user circle"/>
                 </Accordion.Title>
                 <Accordion.Content active = {activeIndex === 1}>
-                    posters
+                    <List>
+                    {userPosts && displayTopPosters(userPosts)}
+                    </List>
                 </Accordion.Content>
 
                 {/* Channel Created By*/}
