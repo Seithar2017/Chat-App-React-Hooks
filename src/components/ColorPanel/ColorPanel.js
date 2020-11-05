2d232b95ee2a3458361463fe6b0b73b99f4ac2dc
Krzysztof Waszkiewicz <kwaszkiewicz94@gmail.com>
2020-11-05 14:28:00 +0100

    add SlidePicker and set as controlled component

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,7 +1,22 @@
-import React from 'react';
-import {Sidebar, Menu, Divider, Button} from 'semantic-ui-react';
+import React, {useState} from 'react';
+import {Sidebar, Menu, Divider, Button, Modal, Icon, Label, Segment} from 'semantic-ui-react';
+import {SliderPicker} from 'react-color';
 
 const ColorPanel = () => {
+
+    const [modal, setModal] = useState(false);
+    const [primary, setPrimary] = useState('#40bfbe');
+    const [secondary, setSecondary] = useState('#40bfbe');
+    const openModal = () => setModal(true);
+    const closeModal = () => setModal(false);
+
+    const handleChangePrimary = (color) => {
+            setPrimary(color.hex)
+    }
+
+    const handleChangeSecondary = (color) => {
+            setSecondary(color.hex)
+    }
     return (  
         <Sidebar
         as={Menu}
@@ -12,7 +27,41 @@ const ColorPanel = () => {
         width='very thin'
         >
             <Divider />
-            <Button icon="add" size="small" color="blue"/>
+            <Button 
+                icon="add" 
+                size="small" 
+                color="blue" 
+                onClick={openModal}
+            />
+
+            {/* Color Picker Modal */}
+            <Modal basic open = {modal} onClose = {closeModal}>
+                <Modal.Header>
+                    Choose App Colors
+                </Modal.Header>
+                
+                <Modal.Content>
+                    <Segment inverted>
+                        <Label content="Primary Color" />
+                        <SliderPicker color = {primary} onChange = {handleChangePrimary}/>
+                    </Segment>
+                    <Segment inverted>
+                        <Label content="Secondary Color"/>
+                        <SliderPicker color = {secondary} onChange = {handleChangeSecondary}/>
+                    </Segment>
+                </Modal.Content>
+
+                <Modal.Actions>
+                    <Button color="green" inverted>
+                        <Icon name="checkmark"/>Save colors
+                    </Button>
+                    
+                    <Button color="red" inverted onClick={closeModal}>
+                        <Icon name="remove"/>Cancel
+                    </Button>
+                </Modal.Actions>
+
+            </Modal>
         </Sidebar>
     );
 }
